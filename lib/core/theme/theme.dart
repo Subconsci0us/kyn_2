4bc59f430ecf6f8d2bb8fe23f46832b63599185d
Subconsci0us <04shayan@gmail.com>
2024-11-29 16:58:02 +0500

    fixed theming issues

--- lib/core/theme/theme.dart
+++ lib/core/theme/theme.dart
@@ -99,7 +99,7 @@ class Palette {
 
 class ThemeNotifier extends StateNotifier<ThemeData> {
   ThemeMode _mode;
-  ThemeNotifier({ThemeMode mode = ThemeMode.dark})
+  ThemeNotifier({ThemeMode mode = ThemeMode.light})
       : _mode = mode,
         super(
           Palette.darkModeAppTheme,
